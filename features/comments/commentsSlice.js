1d615b37ed6410674da1634dc5bcf15438a67978
SameasDiscordxd <joshua.sartin.js@gmail.com>
2023-11-01 19:18:29 -0400

    "Favorites"

--- features/comments/commentsSlice.js
+++ features/comments/commentsSlice.js
@@ -13,25 +13,41 @@ export const fetchComments = createAsyncThunk(
   }
 );
 
+export const postComment = createAsyncThunk(
+  "comments/postComment",
+  async (payload, { dispatch, getState }) => {
+    setTimeout(() => {
+      const { comments } = getState();
+      payload.date = new Date().toISOString();
+      payload.id = comments.commentsArray.length;
+      dispatch(addComment(payload));
+    }, 2000);
+  }
+);
+
 const commentsSlice = createSlice({
   name: "comments",
   initialState: { isLoading: true, errMess: null, commentsArray: [] },
-  reducers: {},
-  extraReducers: (builder) => {
-    builder
-      .addCase(fetchComments.pending, (state) => {
-        state.isLoading = true;
-      })
-      .addCase(fetchComments.fulfilled, (state, action) => {
-        state.isLoading = false;
-        state.errMess = null;
-        state.commentsArray = action.payload;
-      })
-      .addCase(fetchComments.rejected, (state, action) => {
-        state.isLoading = false;
-        state.errMess = action.error ? action.error.message : "Fetch failed";
-      });
+  reducers: {
+    addComment: (state, action) => {
+      state.commentsArray.push(action.payload);
+    },
+  },
+  extraReducers: {
+    [fetchComments.pending]: (state) => {
+      state.isLoading = true;
+    },
+    [fetchComments.fulfilled]: (state, action) => {
+      state.isLoading = false;
+      state.errMess = null;
+      state.commentsArray = action.payload;
+    },
+    [fetchComments.rejected]: (state, action) => {
+      state.isLoading = false;
+      state.errMess = action.error ? action.error.message : "Fetch failed";
+    },
   },
 });
 
+export const { addComment } = commentsSlice.actions;
 export const commentsReducer = commentsSlice.reducer;
